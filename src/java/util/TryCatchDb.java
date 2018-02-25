1145457f35bd4853a7f46e1ecf07c4a825f3b0ba
OkamotoYuki29 <okamoto.macho@gmail.com>
2018-02-25 21:27:54 +0900

    [add]SuperDbクラスのメソッドをtry-catch
    したクラスを作成。
    このクラスを継承して、各Dbクラスを作成

---
+++ src/java/util/TryCatchDb.java
@@ -0,0 +1,47 @@
+package util;
+
+import java.util.List;
+
+/**
+ * SuperDbクラスのメソッドをtry-catchしたクラス
+ * @author 岡本　侑貴
+ * @param <T> 
+ */
+public class TryCatchDb<T> extends SuperDb{
+	
+	public TryCatchDb(Class entityClass) {
+		super(entityClass);
+	}
+
+	
+	public	void add(T obj){		//新規登録
+		try{
+			create(obj);
+		}catch(Exception e){}
+	}
+	public	void update(T obj){		//更新
+		try{
+			edit(obj);
+		}catch(Exception e){}
+		
+	}
+	public	void delete(T obj){		//削除
+		try{
+			remove(obj);
+		}catch(Exception e){}
+	}
+	public	T search(Object id){	//主キー検索
+		return	(T)find(id);
+	}
+	
+	public List<T> getAll() {		//全件検索
+		return	findAll();
+	}
+	public List<T> getRange(int[] range) {		//配列で範囲を限定した主キー検索
+		return	findRange(range);
+	}
+
+	public int dataCount() {		//総件数を返す
+		return	count();
+	}
+}
